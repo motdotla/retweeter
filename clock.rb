109985d0f8ffc697c5c0965dfdc432b2c2ea7789
scottmotte <scott@scottmotte.com>
2012-02-09 00:04:59 -0800

    move to a heroku based config

--- clock.rb
+++ clock.rb
@@ -1,5 +1,8 @@
 include Clockwork
 require './retweeter'
+
+TWEETING_USERNAME = "scottmotte"
+
 every(2.minutes, 'retweet') do
   Retweeter.new(TWEETING_USERNAME).retweet!
 end
--- retweeter.rb
+++ retweeter.rb
@@ -1,11 +1,10 @@
 require 'twitter'
-require './configuration'
 
 Twitter.configure do |config|
-  config.consumer_key       = CONSUMER_KEY
-  config.consumer_secret    = CONSUMER_SECRET
-  config.oauth_token        = OAUTH_TOKEN
-  config.oauth_token_secret = OAUTH_TOKEN_SECRET
+  config.consumer_key       = ENV['CONSUMER_KEY']
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
 end
 
 class Retweeter
